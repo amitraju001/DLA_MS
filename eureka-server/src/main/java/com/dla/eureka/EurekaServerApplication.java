b1ae4eb55e8f74caad59a268db4ff4192dc25c3b
amitraju001 <amit.raju01@gmail.com>
2022-08-20 11:44:02 +0530

    refactored

--- eureka-server/src/main/java/com/dla/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/dla/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package org.cpringcloud.prince.eurekaserver;
+package com.dla.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
