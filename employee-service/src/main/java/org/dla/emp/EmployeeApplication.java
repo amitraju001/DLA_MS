94d7b6b5bb33b7166c9aec87ba74548fcfa7a20e
amitraju001 <amit.raju01@gmail.com>
2022-08-31 09:25:35 +0530

    added hystrix dashboard

--- employee-service/src/main/java/org/dla/emp/EmployeeApplication.java
+++ employee-service/src/main/java/org/dla/emp/EmployeeApplication.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
